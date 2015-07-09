86b3547194a0cd21e03ffa6365264b93b3fd9440
Alina <ananyivaalina@gmail.com>
2015-07-10 00:26:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
-var generateSign = function(){
-var array = ["a", "b", "c", "d", "e", "f", "g", "h", "i","j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];//Работает.
-var getRandomLetter = function () {
-return array[Math.floor(Math.random() * array.length)];//Генерирую случайную букву. Работает.
-};
-letter = getRandomLetter();//Присваиваю переменной функцию. Работает.
-$(document).ready(function(){
-$("span:contains('" + letter + "')").addClass("chosen");//Выделить букву, которую следует ввести синим.
-});
+var generateSign = function() {
+    var array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //Работает.
+    var getRandomLetter = function() {
+        return array[Math.floor(Math.random() * array.length)]; //Генерирую случайную букву. Работает.
+    };
+    letter = getRandomLetter(); //Присваиваю переменной функцию. Работает.
+    $(document).ready(function() {
+        $("span:contains('" + letter + "')").addClass("chosen"); //Выделить букву, которую следует ввести синим.
+    });
 }
 
-var compare = function(){
-$(document).ready(function(){
-var userInput = document.getElementById("inputValue").value;//Получить,введённый пользователем
-if(userInput == letter){//Если пользователь ввёл правильный символ, выделяем его зелёным.
-$("span:contains('" + letter + "')").addClass("correct");
-}else{
-$("span:contains('" + userInput + "')").addClass("incorrect");//Иначе, выделяем введённый символ красным.
-};
-});
-setTimeout(function(){ clear(); }, 500);//Позволю системе отобразить результаты ввода (окрашибание символов в красный или зелёный цвет).
+var compare = function() {
+    $(document).ready(function() {
+        var userInput = document.getElementById("inputValue").value; //Получить,введённый пользователем
+        if (userInput == letter) { //Если пользователь ввёл правильный символ, выделяем его зелёным.
+            $("span:contains('" + letter + "')").addClass("correct");
+        } else {
+            $("span:contains('" + userInput + "')").addClass("incorrect"); //Иначе, выделяем введённый символ красным.
+        };
+    });
+    setTimeout(function() {
+        clear();
+    }, 500); //Позволю системе отобразить результаты ввода (окрашибание символов в красный или зелёный цвет).
 }
 
-var clear = function(){
-$(document).ready(function(){
-document.getElementById("inputValue").value = "";//Очистить поле ввода.
-letter = "";//Присвоить переменным пустую строку.
-userInput = "";
+var clear = function() {
+    $(document).ready(function() {
+        document.getElementById("inputValue").value = ""; //Очистить поле ввода.
+        letter = ""; //Присвоить переменным пустую строку.
+        userInput = "";
 
-if($("span").hasClass("chosen")){//Поудалять все, присвоенные ранее, классы.
-   $("span").removeClass("chosen")
+        if ($("span").hasClass("chosen")) { //Поудалять все, присвоенные ранее, классы.
+            $("span").removeClass("chosen")
+        }
+        if ($("span").hasClass("correct")) {
+            $("span").removeClass("correct")
+        }
+        if ($("span").hasClass("incorrect")) {
+            $("span").removeClass("incorrect")
+        }
+        generateSign(); //Вызвать generateSign().
+    });
 }
-if($("span").hasClass("correct")){
-   $("span").removeClass("correct")
-}
-if($("span").hasClass("incorrect")){
-   $("span").removeClass("incorrect")
-}
-generateSign();//Вызвать generateSign().
-});
-} 
